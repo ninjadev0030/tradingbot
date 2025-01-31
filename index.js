204723aff977f958403969fca082608eed18099f
Ayadi Moni <ayadimoni225@gmail.com>
2025-01-31 07:16:01 -0800

    first commit

---
+++ index.js
@@ -0,0 +1,27 @@
+require("dotenv").config();
+const { Telegraf, Markup } = require("telegraf");
+const Web3 = require("web3");
+
+const bot = new Telegraf(process.env.BOT_TOKEN);
+const web3 = new Web3("https://api.roninchain.com/rpc");
+
+// Main menu buttons
+const mainMenu = Markup.inlineKeyboard([
+  [Markup.button.callback("🔹 Buy", "buy"), Markup.button.callback("🔸 Sell", "sell")],
+  [Markup.button.callback("🔗 Connect Wallet", "connect_wallet"), Markup.button.callback("📋 Copy Trade", "copy_trade")]
+]);
+
+bot.start((ctx) => ctx.reply("Welcome to Ronin Trading Bot!", mainMenu));
+
+bot.action("buy", (ctx) => ctx.reply("Buying on Ronin..."));
+bot.action("sell", (ctx) => ctx.reply("Selling on Ronin..."));
+bot.action("connect_wallet", (ctx) => ctx.reply("Send your Ronin wallet address."));
+bot.action("copy_trade", (ctx) => ctx.reply("Copy trading activated!"));
+
+bot.launch();
+
+// Graceful stop
+process.once("SIGINT", () => bot.stop("SIGINT"));
+process.once("SIGTERM", () => bot.stop("SIGTERM"));
+
+console.log("Bot is running...");
